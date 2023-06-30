41b2b1c5032837fe06ae73443278afaa38a1f54c
boyeon <qhdus96@naver.com>
2023-06-30 15:09:42 +0900

    error 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,6 @@
 export default function Footer() {
   return (
-    <footer
-      className="w-full h-28 mt-10 bg-gray-50 shadow-lg`}>
-      <div "
-    >
+    <footer className="w-full h-28 mt-10 bg-gray-50 shadow-lg">
       <div className="container mx-auto py-4">
         <p className="text-center text-gray-400">
           © 2023 boyeon-blog. All rights reserved.
